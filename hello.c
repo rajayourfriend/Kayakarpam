486bd1348e5fdd0ae708fb612b64217d3def1b31
Raja P <42088982+rajayourfriend@users.noreply.github.com>
2021-02-24 20:43:45 +0530

    primitive codes

---
+++ hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("hello world-C\n");
+    return 0;
+}
---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    std::cout << "hello world-C++" << std::endl;
+}
